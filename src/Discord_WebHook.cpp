6ce0087837a3d216573e592364b32d2feef256b9
Iqbal Rifai <yukifag@pm.me>
2024-11-28 01:26:35 +0700

    adding support hex code color and convert to numeric

--- src/Discord_WebHook.cpp
+++ src/Discord_WebHook.cpp
@@ -1,28 +1,28 @@
 /*
-  Discord_WebHook.cpp
-  Library for sending messages to Discord via WebHook
-
-  Copyright (c) 2024 µsini
-  Author : Rémi Sarrailh
-  Version : 1.1.0
-
-  The MIT License (MIT)
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to
-  deal in the Software without restriction, including without limitation the
-  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-  sell copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-  IN THE SOFTWARE.
-*/
+ *  Discord_WebHook.cpp
+ *  Library for sending messages to Discord via WebHook
+ *
+ *  Copyright (c) 2024 µsini
+ *  Author : Rémi Sarrailh
+ *  Version : 1.1.0
+ *
+ *  The MIT License (MIT)
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *    The above copyright notice and this permission notice shall be included in
+ *    all copies or substantial portions of the Software.
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
 
 #include "Discord_WebHook.h"
 
@@ -77,81 +77,94 @@ bool Discord_Webhook::send(String content)
   return Discord_Webhook::sendRequest("{\"content\":\"" + content + "\"");
 }
 
-bool Discord_Webhook::sendEmbed(String title, String description, String imageUrl, String color)
+bool Discord_Webhook::sendEmbed(String title, String description, String imageUrl, String hexColor)
 {
+  // Remove '#' from the hex color if it exists and convert to numeric
+  long colorValue = strtol(hexColor.startsWith("#") ? hexColor.substring(1).c_str() : hexColor.c_str(), nullptr, 16);
+
   String embedContent = "{\"embeds\":[{\"title\":\"" + title +
   "\",\"description\":\"" + description +
-  "\",\"color\":" + color +
+  "\",\"color\":" + String(colorValue) +
   ",\"image\":{\"url\":\"" + imageUrl + "\"}}]";
   return Discord_Webhook::sendRequest(embedContent);
-}
-
-// Send message to Discord, we disable SSL certificate verification for ease of
-// use (Warning: this is insecure)
-bool Discord_Webhook::sendRequest(String jsonPayload)
-{
-  String discord_tts = "false";
-  if (Discord_Webhook::tts)
-  {
-    discord_tts = "true";
   }
-  WiFiClientSecure *client = new WiFiClientSecure; // Create a WiFiClientSecure
-  bool ok = false;
-  if (client)
+
+  // Send message to Discord, we disable SSL certificate verification for ease of
+  // use (Warning: this is insecure)
+  bool Discord_Webhook::sendRequest(String jsonPayload)
   {
-    client->setInsecure(); // Disable SSL certificate verification
-
-    HTTPClient https; // Create HTTPClient
-    String payload = jsonPayload + ",\"tts\":" + discord_tts + "}";
-    if (Discord_Webhook::debug)
+    String discord_tts = "false";
+    if (Discord_Webhook::tts)
     {
-      Serial.println("[HTTP] Connecting to Discord...");
-      Serial.println("[HTTP] Payload: " + payload);
-      Serial.println("[HTTP] TTS: " + discord_tts);
+      discord_tts = "true";
     }
-
-    // Begin HTTPS requests
-    if (https.begin(*client, Discord_Webhook::webhook_url))
+    WiFiClientSecure *client = new WiFiClientSecure; // Create a WiFiClientSecure
+    bool ok = false;
+    if (client)
     {
-      https.addHeader("Content-Type", "application/json"); // Set request as JSON
+      client->setInsecure(); // Disable SSL certificate verification
 
-      // Send POST request
-      int httpCode = https.POST(payload);
-      if (httpCode > 0)
-      { // if HTTP code is return
-        if (httpCode == HTTP_CODE_OK ||
+      HTTPClient https; // Create HTTPClient
+      String payload = jsonPayload + ",\"tts\":" + discord_tts + "}";
+      if (Discord_Webhook::debug)
+      {
+        Serial.println("[HTTP] Connecting to Discord...");
+        Serial.println("[HTTP] Payload: " + payload);
+        Serial.println("[HTTP] TTS: " + discord_tts);
+      }
+
+      // Begin HTTPS requests
+      if (https.begin(*client, Discord_Webhook::webhook_url))
+      {
+        https.addHeader("Content-Type", "application/json"); // Set request as JSON
+
+        // Send POST request
+        int httpCode = https.POST(payload);
+        if (httpCode > 0)
+        { // if HTTP code is return
+          if (httpCode == HTTP_CODE_OK ||
             httpCode == HTTP_CODE_MOVED_PERMANENTLY ||
             httpCode == HTTP_CODE_NO_CONTENT)
-        {
-          // Discord webhook has changed and our request is not correct, so it
-          // will not send response, so we end without getting a response
-          // https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
-          if (Discord_Webhook::debug)
           {
-            Serial.println("[HTTP] OK");
+            // Discord webhook has changed and our request is not correct, so it
+            // will not send response, so we end without getting a response
+            // https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+            if (Discord_Webhook::debug)
+            {
+              Serial.println("[HTTP] OK");
+            }
+            ok = true;
           }
-          ok = true;
+          else
+          {
+            if (Discord_Webhook::debug)
+            {
+              // This should mainly return an error if token or id is invalid
+              String payload = https.getString();
+              Serial.print("[HTTP] ERROR: ");
+              Serial.println(payload);
+              ok = false;
+            }
+          }
+          https.end();
         }
         else
         {
           if (Discord_Webhook::debug)
           {
-            // This should mainly return an error if token or id is invalid
-            String payload = https.getString();
-            Serial.print("[HTTP] ERROR: ");
-            Serial.println(payload);
+            // This will return an error if the server is unreachable
+            Serial.printf("[HTTP] ERROR: %s\n",
+                          https.errorToString(httpCode).c_str());
             ok = false;
           }
         }
-        https.end();
       }
       else
       {
         if (Discord_Webhook::debug)
         {
-          // This will return an error if the server is unreachable
-          Serial.printf("[HTTP] ERROR: %s\n",
-                        https.errorToString(httpCode).c_str());
+          // This will return an error if request failed
+          Serial.printf("[HTTP] Unable to connect\n");
           ok = false;
         }
       }
@@ -160,21 +173,11 @@ bool Discord_Webhook::sendRequest(String jsonPayload)
     {
       if (Discord_Webhook::debug)
       {
-        // This will return an error if request failed
-        Serial.printf("[HTTP] Unable to connect\n");
+        // This shouldn't happen but anyway it's better to check
+        Serial.println("[HTTP] Unable to create client");
         ok = false;
       }
     }
+    delete client;
+    return ok;
   }
-  else
-  {
-    if (Discord_Webhook::debug)
-    {
-      // This shouldn't happen but anyway it's better to check
-      Serial.println("[HTTP] Unable to create client");
-      ok = false;
-    }
-  }
-  delete client;
-  return ok;
-}
